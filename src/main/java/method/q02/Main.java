4b6133f2a0faa36a96090bd701aa554132dd4d24
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-13 10:10:12 +0900

    Added method folder from JavaBasic to Noa_project

--- src/main/java/method/q02/Main.java
+++
@@ -1,9 +0,0 @@
-package method.q02;
-
-public class Main {
-	public static void main(String[] args) {
-		Introduce introduce = new Introduce();
-		
-		introduce.selfIntroduce("山田");
-	}
-}
--- src/main/java/method/q04/Main.java
+++
@@ -1,9 +0,0 @@
-package method.q04;
-
-public class Main {
-	public static void main(String[] args) {
-		SumNumbers sumNumbers = new SumNumbers();
-		
-		sumNumbers.calculateSum(2, 3);
-	}
-}
