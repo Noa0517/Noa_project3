4f0817e2dfbd5935605d492693a4952394d7ecb9
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-19 10:52:02 +0900

    Added model folder from JavaBasic to Noa_project

--- src/main/java/model/entity/CategoryBean.java
+++ src/main/java/model/entity/CategoryBean.java
@@ -4,15 +4,11 @@ public class CategoryBean {
 	private int categoryId;
 	private String categoryName;
 	
-	public CategoryBean(int categoryId, String categoryName) {
-		this.categoryId = categoryId;
-		this.categoryName = categoryName;
-	}
-	
-	public int getCategoryId() {
-		return categoryId;
-	}
-	public String getCategoryName() {
-		return categoryName;
-	}
+	// ゲッター・セッター
+    public int getCategoryId() { return categoryId; }
+    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
+
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
+
 }
