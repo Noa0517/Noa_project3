b755f12ee25b23c396d9f96ddbfe64e747d1cbf7
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-20 10:07:05 +0900

    Update CategoryBean.java

--- src/main/java/model/entity/CategoryBean.java
+++ src/main/java/model/entity/CategoryBean.java
@@ -1,5 +1,24 @@
 package model.entity;
 
-public class CategoryBean {
+import java.io.Serializable;
 
+public class CategoryBean implements Serializable {
+    private int categoryId;
+    private String categoryName;
+	
+    // 引数なしのコンストラクタ
+    public CategoryBean() {}
+    
+    //引数付きコンストラクタの追加
+    public CategoryBean(int categoryId, String categoryName) {
+    	this.categoryId = categoryId;
+    	this.categoryName = categoryName;
+    }
+    
+    //ゲッター・セッター
+    public int getCategoryId() { return categoryId; }
+    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
+
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
 }
