a6c0eec94407110e9bb92e8df6f89fbb6fb3b481
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-20 10:06:37 +0900

    Update ConnectionManager.java

--- src/main/java/model/dao/ConnectionManager.java
+++ src/main/java/model/dao/ConnectionManager.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+	private static final String URL = "jdbc:mysql://localhost:3306/yourdb";
+	private static final String USER = "Noa";
+	private static final String PASSWORD = "Noa20010517&&";
+	
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
 
 }
