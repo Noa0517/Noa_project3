43a7d714722183cdb799a05244d1fa7bb21958ab
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-20 10:07:42 +0900

    Update CategoryListServlet.java

--- src/main/java/servlet/CategoryListServlet.java
+++ src/main/java/servlet/CategoryListServlet.java
@@ -1,5 +1,32 @@
 package servlet;
 
-public class CategoryListServlet extends HttpServlet {
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dao.CategoryDAO;
+import model.dao.ConnectionManager;
+import model.entity.CategoryBean;
+
+//@WebServlet("/category-list")
+
+public class CategoryListServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	try (Connection connection = ConnectionManager.getConnection()){
+    		CategoryDAO categoryDAO = new CategoryDAO(connection);
+    		List<CategoryBean> categoryList = categoryDAO.getAllCategories();
+    		
+    		request.setAttribute("categoryList", categoryList);
+    		RequestDispatcher dispatcher = request.getRequestDispatcher("category-list.jsp");
+    		dispatcher.forward(request, response);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "データ取得中にエラーが発生しました");
+    	}
+    }
 }
