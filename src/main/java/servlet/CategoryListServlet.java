1e771c0490c202982ca610efbd90d86cd7a8bd3a
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-20 11:44:14 +0900

    Added model folder from JavaBasic to Noa_project

--- src/main/java/servlet/CategoryListServlet.java
+++ src/main/java/servlet/CategoryListServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
@@ -10,23 +9,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dao.CategoryDAO;
-import model.dao.ConnectionManager;
 import model.entity.CategoryBean;
 
 //@WebServlet("/category-list")
 
 public class CategoryListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final CategoryDAO dao = new CategoryDAO();
+	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	try (Connection connection = ConnectionManager.getConnection()){
-    		CategoryDAO categoryDAO = new CategoryDAO(connection);
-    		List<CategoryBean> categoryList = categoryDAO.getAllCategories();
-    		
+    	try {
+    		List<CategoryBean> categoryList = dao.getCategories();
     		request.setAttribute("categoryList", categoryList);
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("category-list.jsp");
-    		dispatcher.forward(request, response);
-    	} catch (Exception e) {
+    	} catch(Exception e) {
     		e.printStackTrace();
-    		response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "データ取得中にエラーが発生しました");
+    		request.setAttribute("errorMessage", "カテゴリ情報の取得に失敗しました。");
     	}
+    	
+        RequestDispatcher dispatcher = request.getRequestDispatcher("category-list.jsp");
+        dispatcher.forward(request, response);
+
     }
 }
