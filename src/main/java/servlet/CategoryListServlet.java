ae200506d20e1918791d3112e47ff2cd54ac371a
Noa0517 <n-fukaya@seassist.co.jp>
2025-06-17 19:41:07 +0900

    Update CategoryListServlet.java

--- src/main/java/servlet/CategoryListServlet.java
+++ src/main/java/servlet/CategoryListServlet.java
@@ -1,7 +1,6 @@
-package servlet;
+package servlet; 
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
@@ -10,23 +9,33 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dao.CategoryDAO;
-import model.dao.ConnectionManager;
 import model.entity.CategoryBean;
 
-//@WebServlet("/category-list")
-
+//登録データを一覧表示するクラス
+//@webServlet("/category-list")
 public class CategoryListServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	try (Connection connection = ConnectionManager.getConnection()){
-    		CategoryDAO categoryDAO = new CategoryDAO(connection);
-    		List<CategoryBean> categoryList = categoryDAO.getAllCategories();
-    		
-    		request.setAttribute("categoryList", categoryList);
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("category-list.jsp");
-    		dispatcher.forward(request, response);
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    		response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "データ取得中にエラーが発生しました");
-    	}
-    }
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+		
+		//DAOオブジェクトを生成
+		CategoryDAO2 sdao =new CategoryDAO2();
+		
+		List<CategoryBean> categoryList = sdao.getAllCategories(); //追加
+		
+		System.out.println("取得したカテゴリリスト：" + categoryList);
+		req.setAttribute("categoryList", categoryList); //追加
+		
+		//全件検索した結果
+		CategoryDAO sdto = sdao.select();
+			
+			req.setAttribute("sdto", sdto);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/category-list.jsp");
+		rd.forward(req, res);
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException{
+		doPost(req, res);
+	}
 }
