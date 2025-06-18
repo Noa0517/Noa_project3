8143f36dd0a979f3ef083101191dcee8c5e59ddd
Noa0517 <n-fukaya@seassist.co.jp>
2025-06-18 19:55:18 +0900

    Update CategoryListServlet.java

--- src/main/java/servlet/CategoryListServlet.java
+++ src/main/java/servlet/CategoryListServlet.java
@@ -8,27 +8,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.dao.CategoryDAO;
 import model.entity.CategoryBean;
 
 //登録データを一覧表示するクラス
 //@webServlet("/category-list")
-public class CategoryListServlet extends HttpServlet {
+public class CategoryListServlet extends HttpServlet {	
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws IOException, ServletException {
 		
 		//DAOオブジェクトを生成
 		CategoryDAO2 sdao =new CategoryDAO2();
+		List<CategoryBean> categoryList = sdao.select(); //追加
 		
-		List<CategoryBean> categoryList = sdao.getAllCategories(); //追加
-		
-		System.out.println("取得したカテゴリリスト：" + categoryList);
 		req.setAttribute("categoryList", categoryList); //追加
 		
 		//全件検索した結果
-		CategoryDAO sdto = sdao.select();
+		//CategoryDAO sdto = sdao.select();
 			
-			req.setAttribute("sdto", sdto);
+			//req.setAttribute("sdto", sdto);
 		
 		RequestDispatcher rd = req.getRequestDispatcher("/category-list.jsp");
 		rd.forward(req, res);
