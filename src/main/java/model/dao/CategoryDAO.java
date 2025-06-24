bdec0b2d1e487cb3f42faa64cff2ea15926d93c7
Noa0517 <n-fukaya@seassist.co.jp>
2025-06-24 20:22:28 +0900

    Added model folder from JavaBasic to Noa_project

--- src/main/java/model/dao/CategoryDAO.java
+++ src/main/java/model/dao/CategoryDAO.java
@@ -1,8 +1,8 @@
 package model.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,60 +10,26 @@ import java.util.List;
 import model.entity.CategoryBean;
 
 public class CategoryDAO {
-	private static final String URL = "jdbc:mysql://localhost:3306/categories?serverTimezone=UTC&useSSL=false";
-	//private static final String URL = "jdbc:mysql://localhost:3306/categories?serverTimezone=UTC";
-	private static final String USER = "root";
-	private static final String PASS = "Noa20010517&&";
-	private Connection con = null;
-	
-	public void connect() {
-		try {
-			//DBに接続
-			con = DriverManager.getConnection(URL, USER, PASS);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public List<CategoryBean> select() {
-		Statement stmt = null;
-		ResultSet rs = null;
 		List<CategoryBean> categoryList = new ArrayList<>();
-		String sql = "SELECT * FROM categories;";
+		String sql = "SELECT * FROM categories";
 		
-		try {
-			connect();
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(sql);
+		//try-with-resource文でリソースを自動管理
+		try (Connection con = ConnectionManager.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
 			
 			while (rs.next()) {
-				CategoryBean sb = new CategoryBean();
-				sb.setId(rs.getInt("category_id"));
-				sb.setName(rs.getString("category_name"));
-				categoryList.add(sb);
+				CategoryBean category = new CategoryBean();
+				category.setId(rs.getInt("category_id"));
+				category.setName(rs.getString("category_name"));
+				categoryList.add(category);
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (stmt != null)
-					stmt.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			
+		} catch (SQLException e) {
+			System.err.println("データベースエラー：" + e.getMessage());
 		}
-		disconnect();
+		
 		return categoryList;
-	}
-	
-	public void disconnect() {
-		try {
-			if(con != null)
-				con.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
