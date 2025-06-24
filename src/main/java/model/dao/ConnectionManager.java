364fb041464b9f12a1c0ed09a3d498d588a3b633
Noa0517 <n-fukaya@seassist.co.jp>
2025-06-24 20:24:00 +0900

    Update ConnectionManager.java

--- src/main/java/model/dao/ConnectionManager.java
+++ src/main/java/model/dao/ConnectionManager.java
@@ -1,24 +1,36 @@
 package model.dao;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import model.entity.CategoryBean;
+/**
+ * データベース接続を管理するクラス
+ */
 
-public class ConnectionManager implements Serializable {
-	private ArrayList<CategoryBean> list;
+public class ConnectionManager {
 	
-	public ConnectionManager() {
-		list = new ArrayList<CategoryBean>();
+	private static final String URL = "jdbc:mysql://localhost:3306/categories?serverTimezone=UTC&useSSL=false";
+	//private static final String URL = "jdbc:mysql://localhost:3306/categories?serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PASS = "Noa20010517&&";
+	
+	//JDBCドライバのロード(静的初期化ブロック)
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("JDBCドライバのロードに失敗しました。", e);
+		}
 	}
-	public void add(CategoryBean sb) {
-		list.add(sb);
+	
+	/**
+	 * データベース接続を取得する
+	 * @return Connection データベース接続
+	 * @throws SQLException データベース接続エラー
+	 */
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASS);
 	}
-	public CategoryBean get(int i) {
-		return list.get(i);
-	}
-	public int size() {
-		return list.size();
-	}
-
 }
