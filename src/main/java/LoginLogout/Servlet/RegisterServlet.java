5ab641f88b25ce0aaf85f78b0b6103498413f503
Noa0517 <n-fukaya@seassist.co.jp>
2025-10-05 18:45:10 +0900

    Added LoginLogout folder from JavaBasic to Noa_project

--- src/main/java/LoginLogout/Servlet/RegisterServlet.java
+++ src/main/java/LoginLogout/Servlet/RegisterServlet.java
@@ -1,9 +1,13 @@
 package LoginLogout.Servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import LoginLogout.dao.CategoryDAO;
 import LoginLogout.dao.ProductDAO;
+import LoginLogout.model.Category;
 import LoginLogout.model.Product;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +16,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/add-product")
 public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+
+	    CategoryDAO categoryDao = new CategoryDAO();
+	    List<Category> categories = categoryDao.findAll();
+	    request.setAttribute("categories", categories);
+
+	    RequestDispatcher dispatcher = request.getRequestDispatcher("addProduct.jsp");
+	    dispatcher.forward(request, response);
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
@@ -24,6 +41,7 @@ public class RegisterServlet extends HttpServlet {
 		String name = request.getParameter("name");
 		String priceStr = request.getParameter("price");
 		String stockStr = request.getParameter("stock");
+		int stock = Integer.parseInt(request.getParameter("stock"));
 		String categoryIdStr = request.getParameter("categoryId");
 
 	    
@@ -33,13 +51,19 @@ public class RegisterServlet extends HttpServlet {
 						categoryIdStr == null || categoryIdStr.isEmpty()) {
 			
 			request.setAttribute("errorMessage", "すべての項目を入力してください");
+			
+			//ここでカテゴリ一覧を渡す
+		    CategoryDAO categoryDao = new CategoryDAO();
+		    List<Category> categories = categoryDao.findAll();
+		    request.setAttribute("categories", categories);
+		    
 			request.getRequestDispatcher("addProduct.jsp").forward(request, response);
-			return;
+			//return;
 		}
 		
 		//数値チェック
 		int price = Integer.parseInt(priceStr);
-	    int stock = Integer.parseInt(stockStr);
+	    //int stock = Integer.parseInt(stockStr);
 	    int categoryId = Integer.parseInt(categoryIdStr);
 	    
 	    //マイナスチェック
@@ -50,6 +74,7 @@ public class RegisterServlet extends HttpServlet {
 	    }
 		
 		Product product = new Product(name, price, stock, categoryId);
+		//Product product = new Product(productId,name, price, stock, categoryId);
 		ProductDAO dao = new ProductDAO();
 		dao.insert(product);
 	    
