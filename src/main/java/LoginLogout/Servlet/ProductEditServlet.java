6c5ddbc3f487b40bb043cb8996284ca715394088
Noa0517 <n-fukaya@seassist.co.jp>
2025-10-08 07:16:19 +0900

    Update ProductEditServlet.java

--- src/main/java/LoginLogout/Servlet/ProductEditServlet.java
+++ src/main/java/LoginLogout/Servlet/ProductEditServlet.java
@@ -1,8 +1,11 @@
 package LoginLogout.Servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import LoginLogout.dao.CategoryDAO;
 import LoginLogout.dao.ProductDAO;
+import LoginLogout.model.Category;
 import LoginLogout.model.Product;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -14,14 +17,21 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/product-edit")
 public class ProductEditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    
             throws ServletException, IOException {
         String idStr = request.getParameter("id");
         int id = Integer.parseInt(idStr);
 
         ProductDAO dao = new ProductDAO();
         Product product = dao.findById(id);
-
         request.setAttribute("product", product);
+        
+        //カテゴリ一覧を取得して渡す！
+        CategoryDAO categoryDao = new CategoryDAO();
+        List<Category> categories = categoryDao.findAll();
+        request.setAttribute("categories", categories);
+
+        //request.setAttribute("product", product);
         RequestDispatcher dispatcher = request.getRequestDispatcher("product-edit.jsp");
         dispatcher.forward(request, response);
     }
